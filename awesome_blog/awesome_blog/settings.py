d535007bb1ab27059372ad9148b2e69cac5db225
Arsture <minseok.kim@pygmalion.team>
2024-02-28 10:09:13 +0900

    feature: user 기초세팅 완

--- awesome_blog/awesome_blog/settings.py
+++ awesome_blog/awesome_blog/settings.py
@@ -35,8 +35,21 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
     'user',
+    'comment',
+    'post',
+    'tag',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ]
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- awesome_blog/user/views.py
+++ awesome_blog/user/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import generics
+from .serializers import UserSerializer
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.permissions import IsAdminUser
 
-# Create your views here.
+User = get_user_model()
+
+
+class UserListCreateAPIView(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    # TODO: permission_classes 설정
+
+
+class UserRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    authentication_classes = [SessionAuthentication, BasicAuthentication]
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    # TODO: permission_classes 설정
